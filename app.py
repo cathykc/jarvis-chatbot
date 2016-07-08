10105a8340a2b6d9bf2fe0928147529e09009442
Cathy Chen <cathychenn@gmail.com>
2016-07-08 05:04:41 -0700

    [chatbot] creating message creation functions, commenting

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def dashboard():
     return render_template('dashboard.html')
 
+
+
+# *****************************************************************************
+# CHATBOT WEBHOOK
+# *****************************************************************************
+
+# please excuse pulic keys - to move soon
 APP_SECRET = "763bc897c7ab402b870ad33a7cd59062"
 VALIDATION_TOKEN = "jarvis"
 PAGE_ACCESS_TOKEN = "EAANTFr9A1IEBAFi3QsRXDkZBl5yVYZC5XrCuqUxZCXDcc2Y9rD3LEqAtdqhpNHZAfZAWUVCzh9XmZCKcTV3ZBPIuH4ChfqfYaIkha2zLsazbyxoB8vJKFwr0qbwtwO7lbZBsiOgXfGjKq5zTmJvKrmnxKYqkmZCRZAnv1XKqlZCK4cnEQZDZD"
@@ -28,7 +35,7 @@ def webhook():
                     elif 'delivery' in message:
                         pass
                     elif 'postback' in message:
-                        pass
+                        receivedPostback(message)
                     else:
                         print "Webhook received unknown message."
 
@@ -39,7 +46,7 @@ def webhook():
     elif request.method == 'GET':
         print "get called"
         if (request.args.get('hub.mode') == 'subscribe' and
-        request.args.get('hub.verify_token') == VALIDATION_TOKEN):
+                request.args.get('hub.verify_token') == VALIDATION_TOKEN):
             return request.args.get('hub.challenge'), 200
         else:
             print "**********"
@@ -47,6 +54,11 @@ def webhook():
             print request.args.get('hub.verify_token')
             print request
             return "Wrong Verify Token", 403
+
+
+# *****************************************************************************
+# CHATBOT MESSAGING AND POSTBACKS
+# *****************************************************************************
 
 
 def receivedMessage(event):
@@ -59,11 +71,96 @@ def receivedMessage(event):
         sendTextMessage(senderID, "Attachment received.")
 
 
+def receivedPostback(event):
+    senderID = event['sender']['id']
+    sendTextMessage(senderID, "Postback called.")
+
+
+# *****************************************************************************
+# CHATBOT MESSAGES
+# *****************************************************************************
+
+# Send a text mesage.
 def sendTextMessage(recipientId, messageText):
 
-    messageData = {}
-    messageData['recipient'] = {'id': recipientId}
+    messageData = {'recipient': recipientId}
     messageData['message'] = {'text': messageText}
+
+    callSendAPI(messageData)
+
+
+# Send an image message.
+def sendImageMessage(recipientId, imageUrl):
+    messageData = {'recipient': recipientId}
+
+    attachment = {'type': "image"}
+    attachment['payload'] = {'url': imageUrl}
+    messageData['message'] = {'attachment': attachment}
+
+    callSendAPI(messageData)
+
+
+# Send a button message.
+# e.g. buttonList:
+# [{
+#     type: "web_url",
+#     url: "https://www.oculus.com/en-us/rift/",
+#     title: "Open Web URL"
+# }, {
+#     type: "postback",
+#     title: "Call Postback",
+#     payload: "Developer defined postback"
+# }]
+def sendButtonMessage(recipientId, messageText, buttonList):
+    messageData = {'recipient': recipientId}
+
+    attachment = {'type': "template"}
+    attachment['payload'] = {'template_type': button,
+                             'text': messageText,
+                             'buttons': buttonList}
+    messageData['message'] = {'attachment': attachment}
+
+    callSendAPI(messageData)
+
+
+# Send a Carousel Message.
+# e.g. elementList:
+# [{
+#     title: "rift",
+#     subtitle: "Next-generation virtual reality",
+#     item_url: "https://www.oculus.com/en-us/rift/",               
+#     image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+#     buttons: [{
+#         type: "web_url",
+#         url: "https://www.oculus.com/en-us/rift/",
+#         title: "Open Web URL"
+#     }, {
+#         type: "postback",
+#         title: "Call Postback",
+#         payload: "Payload for first bubble",
+#     }],
+# }, {
+#     title: "touch",
+#     subtitle: "Your Hands, Now in VR",
+#     item_url: "https://www.oculus.com/en-us/touch/",               
+#     image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+#     buttons: [{
+#         type: "web_url",
+#         url: "https://www.oculus.com/en-us/touch/",
+#         title: "Open Web URL"
+#     }, {
+#         type: "postback",
+#         title: "Call Postback",
+#         payload: "Payload for second bubble",
+#     }]
+# }]
+def sendCarouselMessage(recipientId, elementList):
+    messageData = {'recipient': recipientId}
+
+    attachment = {'type': "template"}
+    attachment['payload'] = {'template_type': "generic",
+                             'elements': elementList}
+    messageData['message'] = {'attachment': attachment}
 
     callSendAPI(messageData)
 
