8c12d42231b7bcd5a82ad8e459382517681dd9d2
Elizabeth Hamp <ehamp@seas.upenn.edu>
2016-07-09 16:28:00 -0700

    nyt links

--- api/nyt_api.py
+++ api/nyt_api.py
@@ -7,21 +7,21 @@ def get_top_articles():
           "/all-sections/1.json"
     url += '?' + "api-key=4149d1e3df254bc1b8bac5dca3ba38cb"
     r = requests.get(url)
+
     for i in range(0, 5):
         image_url = r.json()['results'][i]['media'][0]['media-metadata'][0]['url']
-        # print image_url
-        # print r.json()['results'][i]['title']
-        # print r.json()['results'][i]['url']
-        print "made it here"
+        abstract = r.json()['results'][i]['abstract']
         article = {}
         article["title"] = r.json()['results'][i]['title']
-        article["subtitle"] = "temp"
+        article["subtitle"] = abstract
         article["item_url"] = r.json()['results'][i]['url']
         article["image_url"] = image_url
 
         buttons = [{"type": "postback", "title" : "Read here",
                     "payload"
-                    : "payload for yelp homies"}]
+                    : "nyt payload"}]
         article["buttons"] = buttons
         articles.append(article)
     return articles
+
+get_top_articles()
