6f208280d7c8afb1f707fe1c51f8fdb83da5965e
Elizabeth Hamp <ehamp@seas.upenn.edu>
2016-07-09 16:18:50 -0700

    fixing news

--- api/nyt_api.py
+++ api/nyt_api.py
@@ -1,11 +1,10 @@
 import requests
 
 
-
-
 def get_top_articles():
     articles = []
-    url = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json"
+    url = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed" \
+          "/all-sections/1.json"
     url += '?' + "api-key=4149d1e3df254bc1b8bac5dca3ba38cb"
     r = requests.get(url)
     for i in range(0, 5):
@@ -13,7 +12,7 @@ def get_top_articles():
         # print image_url
         # print r.json()['results'][i]['title']
         # print r.json()['results'][i]['url']
-
+        print "made it here"
         article = {}
         article["title"] = r.json()['results'][i]['title']
         article["subtitle"] = "temp"
