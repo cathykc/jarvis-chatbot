28a9825fa8daa1d6488ff431fb678d75322a3769
Elizabeth Hamp <ehamp@seas.upenn.edu>
2016-07-10 08:49:58 -0700

    paylod

--- manage.py
+++ manage.py
@@ -473,7 +473,7 @@ def receivedPostback(event):
     facebook_id = event['sender']['id']
     payload = event['postback']['payload']
 
-    if payload is None or payload == "noop":
+    if payload is None or payload == "noop" or payload == "SCHEDULE_EVENT":
         return
 
     if payload == "GET_STARTED":
